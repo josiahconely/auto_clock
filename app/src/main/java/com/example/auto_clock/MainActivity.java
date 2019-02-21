bce88fea15281e381abd0018c3d0d4068ac69654
josiahconely <josiahconely@gmail.com>
2019-02-21 09:59:58 -0600

    initial Main activity ui

--- app/src/main/java/com/example/auto_clock/MainActivity.java
+++ app/src/main/java/com/example/auto_clock/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.auto_clock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //sets date and time for Display
+        Calendar calender = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calender.getTime());
+        String currentTime = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(calender.getTime());
+        TextView textView_date = findViewById(R.id.text_view_date);
+        TextView textView_time = findViewById(R.id.text_view_time);
+        textView_date.setText(currentDate);
+        textView_time.setText(currentTime);
     }
 }
