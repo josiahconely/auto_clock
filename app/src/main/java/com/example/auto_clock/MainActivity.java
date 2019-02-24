31128a78527cc5a726cdd2bfdc438620239e9922
josiahconely <josiahconely@gmail.com>
2019-02-23 18:04:46 -0600

    code clean up and commenting

--- app/src/main/java/com/example/auto_clock/MainActivity.java
+++ app/src/main/java/com/example/auto_clock/MainActivity.java
@@ -58,17 +58,27 @@ public class MainActivity extends AppCompatActivity {
         TextView textView_status = findViewById(R.id.text_view_status);
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
+        //checks the isClockedIn flag to determin what to do
         if(!(sharedpreferences.getBoolean("isClockedIn", false))) {
+            //gets the current time
             Calendar calender = Calendar.getInstance();
+            //updates text_view_status to clocked in
             textView_status.setText("Clocked In");
 
+            /*saves an "in_time" to preferences so that it can be added to the database when
+            clock out is clicked*/
             sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedpreferences.edit();
             editor.putString("in_time", calender.getTime().toString());
-            //updates the in out flag
+            //updates the isClockedIn flag
             editor.putBoolean("isClockedIn", true);
             editor.commit();
+
+            //updates the Last_Clock_In_field field on screen
+            TextView textView = findViewById(R.id.Last_Clock_In_field);
+            textView.setText(calender.getTime().toString());
         }
+        //if already clocked in updates text_view_status to:
         else{textView_status.setText("Already Clocked In");}
 
     }
@@ -76,25 +86,26 @@ public class MainActivity extends AppCompatActivity {
     public void OnClickClockOut (View v) {
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         TextView textView_status = findViewById(R.id.text_view_status);
+
+        //checks the isClockedIn flag to determin what to do
         if (sharedpreferences.getBoolean("isClockedIn", false)) {
             //gets the current time
-
             Calendar calender = Calendar.getInstance();
-            //updates the
-
+            //updates text_view_status to clocked out
             textView_status.setText("Clocked Out");
 
-
+            // collects the in_time from saved prefrences
             String in_time = sharedpreferences.getString("in_time", "error, no in_time");
+            //updates the isClockedIn preference
             SharedPreferences.Editor editor = sharedpreferences.edit();
             editor.putBoolean("isClockedIn", false);
             editor.commit();
 
+            // creates sql update
             String sql;
-            sql = String.format("INSERT INTO inOutLog VALUES('%1$s' , '%2$s');", in_time, calender.getTime().toString());
+            //enters into database a new row of in,out time
             SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("clockInOut.db", MODE_PRIVATE, null);
-            sqLiteDatabase.execSQL(sql);
-            sql = "CREATE TABLE IF NOT EXISTS inOutLog(log_id INT, in_time TEXT, out_time TEXT);";
+            sql = String.format("INSERT INTO inOutLog VALUES('%1$s' , '%2$s');", in_time, calender.getTime().toString());
             sqLiteDatabase.execSQL(sql);
 
             //displays Toast text of log info for user
@@ -106,11 +117,16 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "In = " + in + " Out = " + out, Toast.LENGTH_LONG).show();
                 } while (query.moveToNext());
             }
+            //closes database
             query.close();
             sqLiteDatabase.close();
-        }
-        else{textView_status.setText("Already Clocked Out");}
 
+            //updates the last clock out field in view
+            TextView textView = findViewById(R.id.Last_Clock_Out_field);
+            textView.setText(calender.getTime().toString());
+        }
+        //if clocked out pressed twice
+        else{textView_status.setText("Already Clocked Out");}
     }
 }
 
